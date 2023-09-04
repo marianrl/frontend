fc1d5b4a755d7fcdf07d839452377e03c5b5971c
marianrl <marianoh.rodriguez@davinci.edu.ar>
2023-09-04 20:32:16 -0300

    Agregamos SessionProvider para manejar sesiones

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import Buttongroup from "../../components/buttongroup";
+import Button from "../../components/button";
+import {useSession} from "../../components/sessionprovider";
+import { useNavigate } from "react-router-dom";
 
 const Home: React.FC = () => {
+
+    const { logout } = useSession();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout(); // Cierra la sesión
+        navigate('/login'); // Redirige al usuario a la página de inicio de sesión
+    };
+
     return (
-        <div></div>
+        <div>
+            <Buttongroup>
+                <Button onClick={handleLogout} type="button" label="Cerrar Sesion"/>
+            </Buttongroup>
+        </div>
     );
 }
 
